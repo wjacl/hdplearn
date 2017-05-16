95d2b6d1a944d5815b0dd196d6f2a931d7a0644d
wjacl <wang_jian_an@139.com>
2017-05-17 07:56:10 +0800

    ds

---
+++ hdplearn/src/main/java/com/wja/hadoopLearn/hdfs/HdfsDemo.java
@@ -0,0 +1,26 @@
+package com.wja.hadoopLearn.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+
+/**
+ * 
+ * HDFS 文件系统操作示例
+ * 
+ * @author wja
+ */
+public class HdfsDemo
+{
+    static Configuration conf = new Configuration();
+    
+    public static void mkdir(String f)
+    {
+    
+    }
+    
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
