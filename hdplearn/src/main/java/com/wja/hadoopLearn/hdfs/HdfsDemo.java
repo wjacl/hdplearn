a7cf6407b5df950b22efffce553b640ac08a576e
wjacl <wang_jian_an@139.com>
2017-05-22 07:58:54 +0800

    完成

--- hdplearn/src/main/java/com/wja/hadoopLearn/hdfs/HdfsDemo.java
+++ hdplearn/src/main/java/com/wja/hadoopLearn/hdfs/HdfsDemo.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
@@ -129,6 +130,9 @@ public class HdfsDemo
         System.out.println("文件 " + fn + (fs.exists(f) ? " 存在。" : " 不存在。"));
     }
     
+    /**
+     * 从本地拷贝文件、目录 注意：拷贝目录时会用到本地方法，需下载hadoop.dll放到c/windows/System32下。
+     */
     public static void copyFromLocalFile(String source, String dest)
         throws IOException
     {
@@ -138,6 +142,62 @@ public class HdfsDemo
         Path d = new Path(dest);
         fs.copyFromLocalFile(s, d);
         System.out.println("从本地文件 " + source + " 拷贝到 " + dest + " 完成。");
+    }
+    
+    /**
+     * 从hdfs拷贝文件、目录到本地
+     */
+    public static void copyToLocal(String source, String dest)
+        throws IOException
+    {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        Path s = new Path(source);
+        Path d = new Path(dest);
+        fs.copyToLocalFile(s, d);
+        System.out.println("从hdfs拷贝 " + source + " 到 " + dest + " 完成。");
+    }
+    
+    /**
+     * 从本地移动文件、目录到hdfs
+     */
+    public static void moveFromLocal(String source, String dest)
+        throws IOException
+    {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        Path s = new Path(source);
+        Path d = new Path(dest);
+        fs.moveFromLocalFile(s, d);
+        System.out.println("从本地移动 " + source + " 到 " + dest + " 完成。");
+    }
+    
+    /**
+     * 从hdfs移动文件、目录到本地
+     */
+    public static void moveToLocal(String source, String dest)
+        throws IOException
+    {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        Path s = new Path(source);
+        Path d = new Path(dest);
+        fs.moveToLocalFile(s, d);
+        System.out.println("从HDFS移动 " + source + " 到 " + dest + " 完成。");
+    }
+    
+    public static void listStatus(String fn)
+        throws IOException
+    {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        Path f = new Path(fn);
+        System.out.println("listStatus：");
+        FileStatus[] cfs = fs.listStatus(f);
+        for (FileStatus cf : cfs)
+        {
+            System.out.println(cf);
+        }
     }
     
     /**
@@ -168,7 +228,12 @@ public class HdfsDemo
         readFile(fn);
         // deleteFile(fn);
         // exists(fn);
-        
+        // copyFromLocalFile("D:/javaLeanring/javaio/", "/user/wja/javaio/bb");
+        // readFile("/user/wja/javaio/student1");
+        // copyToLocal("/user/wja/javaio/bb", "D:/javaLeanring/aab");
+        // moveFromLocal("D:/javaLeanring/aab", "/user/wja/javaio");
+        // moveToLocal("/user/wja/javaio/aab", "D:/javaLeanring");
+        listStatus("/user/wja/javaio/bb");
         // deleteFile("/user/wja");
         // exists("/user/wja");
         
