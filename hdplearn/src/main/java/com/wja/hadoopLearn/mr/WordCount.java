9ff96be08da741cd40ff2a18940e53e0e63b7b4d
wjacl <wang_jian_an@139.com>
2017-05-26 16:07:29 +0800

    ss

--- hdplearn/src/main/java/com/wja/hadoopLearn/mr/WordCount.java
+++ hdplearn/src/main/java/com/wja/hadoopLearn/mr/WordCount.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.Path; 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
